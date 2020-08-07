1a2c080658cc48b0da3a4eedd22c94738f958acf
Joe <jm16577@my.bristol.ac.uk>
2020-08-07 17:15:27 +0100

    Added ability to keep track of neighbours, adding forces WIP

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Colour { red, green, blue }
+
+    public Colour colour;
+    public AnimationCurve redReaction;
+    public AnimationCurve blueReaction;
+    public AnimationCurve greenReaction;
+
+    //private List<Color> colours = new List<Color> { Color.red, Color.green, Color.blue}; //change to hash map with enum as key?
+    private Dictionary<Colour, Color> colours = new Dictionary<Colour, Color> { {Colour.red, Color.red}, {Colour.green, Color.green }, {Colour.blue, Color.blue} };
+    private Dictionary<Colour, AnimationCurve> colourReactions;
+
+    private Color currentColour;
+
+    private MeshRenderer meshRenderer = null;
+    private Collider collider = null;
+    private Rigidbody rigidbody = null;
+    private List<Cell> affectingCells = new List<Cell>();
+
+    private void Awake()
     {
-        
+        colourReactions = new Dictionary<Colour, AnimationCurve> { { Colour.red, redReaction }, { Colour.green, blueReaction }, { Colour.blue, greenReaction } };
+        currentColour = colours[colour];
+
+        meshRenderer = GetComponent<MeshRenderer>();
+        collider = GetComponent<Collider>();
+        rigidbody = GetComponent<Rigidbody>();
+        meshRenderer.material.color = currentColour;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //List<Cell> instanceAffectingCells = affectingCells; Might need to instantiate due to modifications being done elsewhere!
+        foreach(Cell cell in affectingCells)
+        {
+            Influence(cell.currentColour); 
+            cell.Influence(this.currentColour);
+        }
+
+        //TODO: add a bit of friction
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Cell>() is Cell cell)
+        {
+            affectingCells.Add(cell);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Cell>() is Cell cell)
+        {
+            if (affectingCells.Contains(cell)) 
+            {
+                affectingCells.Remove(cell);
+            }
+        }
+    }
+
+    private void Influence(Color colour)
+    {
+        //TODO
     }
 }
